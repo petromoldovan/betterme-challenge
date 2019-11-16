24435ff2ad6774d25c545b2b57e96cbcbacf7812
petromoldovan <petro.moldovan@dcso.de>
2019-11-16 23:48:39 +0100

    fix issue with multiple fetches

--- src/data/ReposCache.js
+++ src/data/ReposCache.js
@@ -28,25 +28,27 @@ const initialStore = {
 const API_ENDPOINT = 'https://api.github.com/search/repositories'
 
 class ReposCacheClass {
+  store = new BehaviorSubject(initialStore)
+  state$ = this.store.asObservable()
+  repos$ = this.state$.pipe(map(s => s.repos), distinctUntilChanged())
+  pagination$ = this.state$.pipe(map(s => s.pagination), distinctUntilChanged())
+  orderBy$ = this.state$.pipe(map(s => s.orderBy), distinctUntilChanged())
+  q$ = this.state$.pipe(map(s => s.q), distinctUntilChanged())
+
   constructor() {
     this.api = API_ENDPOINT
 
-    this.store = new BehaviorSubject(initialStore)
-    this.state$ = this.store.asObservable()
-    this.repos$ = this.state$.pipe(map(s => s.repos, distinctUntilChanged()))
-    this.pagination$ = this.state$.pipe(map(s => s.pagination, distinctUntilChanged()))
-    this.orderBy$ = this.state$.pipe(map(s => s.orderBy, distinctUntilChanged()))
-    this.q$ = this.state$.pipe(map(s => s.q, distinctUntilChanged()))
-
+    // fetch whenever one of the observables emits a new value
     combineLatest(this.q$, this.pagination$, this.orderBy$)
       .pipe(
         skipWhile(([q]) => q === ''),
+        debounceTime(0),
         switchMap(([q, pagination, orderBy]) => {
           return createCancelableHttp$(`${this.api}${buildRequestParams({q, pagination, orderBy})}`)
         })
       )
       .subscribe(res => {
-        this.updateStore({ repos: res, loading: false })
+        this.updateStore({ repos: res })
       })
   }
 
@@ -62,7 +64,10 @@ class ReposCacheClass {
         debounceTime(300),
       )
     return changeEvent$.subscribe({
-      next: q => this.updateStore({q})
+      next: q =>{
+        console.log('new q emited')
+        this.updateStore({q})
+      }
     })
   }
 
--- src/data/utils.js
+++ src/data/utils.js
@@ -40,7 +40,7 @@ const createCancelableHttp$ = (url, opt = {method: HTTP_METHODS.GET}) => {
 const buildRequestParams = (params) => {
   let url = `?order=${get(params, 'orderBy.order', '')}&sort=${get(params, 'orderBy.sort', '')}&page=${get(params, 'pagination.page', '')}&per_page=${get(params, 'pagination.per_page', '')}`
   if (get(params, 'q')) {
-    url += `&q=${get(params, 'q')}+in:name&`
+    url += `&q=${get(params, 'q')}+in:name`
   }
   return url
 }
